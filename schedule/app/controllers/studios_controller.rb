854cb7338765cabeaff4032c6e11b8499622104a
rachelbookhout <rbookhou@gmail.com>
2015-01-10 15:04:38 -0500

    Created Studio New page

--- schedule/app/controllers/studios_controller.rb
+++ schedule/app/controllers/studios_controller.rb
@@ -1,3 +1,14 @@
 class StudiosController < ApplicationController
+  def new
+    @studio = Studio.new
+  end
 
+  def create
+    @studio = Studio.new(studio_params)
+    if @studio.save
+      redirect_to @studio
+    else
+      render :new
+    end
+  end
 end
